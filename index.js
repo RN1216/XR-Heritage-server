a30c9daf084b35448ad93f782949adde435256af
RN1216 <reunnoor@gmail.com>
2022-11-10 18:38:36 +0600

    set service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -16,8 +16,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+
+    try{
+        const servicesCollection = client.db('xr-heritage').collection('servises');
 
 
+        app.get('/servises',async(req,res)=>{
+            const query = {}
+            const cursor = servicesCollection.find(query);
+            const servises = await cursor.toArray();
+            res.send(servises);
+        }); 
+
+        app.get('/servises/:id',async(req,res)=>{
+            const id =req.params.id;
+            const query = {_id: ObjectId(id)};
+            const servises = await servicesCollection.findOne(query);
+            res.send(servises);
+        });
+
+        app.post('/orders',async (req,res)=>{
+            const order =req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(err=>{console.log(err)})
 app.get('/',(req ,res)=>{
     res.send('XR is running')
 })
